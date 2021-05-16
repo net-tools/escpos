6f57aa99799ab3186f24e143d34324a0aa0cdb6e
Pierre <dev@nettools.ovh>
2021-05-16 20:35:52 +0200

    first commit

---
+++ src/EscPosHelper.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * EscPosHelper
+ *
+ * @author Pierre - dev@nettools.ovh
+ * @license MIT
+ */
+
+
+
+// namespace
+namespace Nettools\EscPos;
+
+
+
+use Nettools\Core\Helpers\ImagingHelper;
+
+
+
+
+/**
+ * Helper class to deal with escpos
+ */
+class EscPosHelper {
+	
+	/**
+	 * Get data bytes for an image ready to be sent to ESCPOS printer
+	 *
+	 * @param resource $image
+	 * @param int $printerResolution X-resolution of printer
+	 * @param float $dither Quantity of dither for black/white conversion
+	 * @return string Return a string to be sent to printer
+	 */
+	public function getImageBytes($image, $printerResolution, $dither = 0.8)
+	{
+		if ( imagesx($image) > 576 )
+			$image = ImagingHelper::image_resize($image, imagesx($image), imagesy($image), $printerResolution, NULL);
+
+
+		// create a gd indexed color converter
+		$converter = new \GDIndexedColorConverter();
+
+		// the color palette
+		$palette = array(
+			array(0, 0, 0),
+			array(255, 255, 255)
+		);
+
+		// convert the image to indexed color mode
+		$new_image = $converter->convertToIndexedColor($image, $palette, 0.8);
+
+		// save the new image
+		$tmp = tempnam(sys_get_temp_dir(), 'escpos-helper-dither');
+		imagepng($new_image, $tmp, 0);
+
+		try
+		{
+			$connector = new \Mike42\Escpos\PrintConnectors\DummyPrintConnector();
+			$printer = new \Mike42\Escpos\Printer($connector);
+
+			try
+			{
+				$img = \Mike42\Escpos\EscposImage::load($tmp, false);
+				$printer->graphics($img);				
+				return $connector->getData();
+			}
+			finally
+			{
+				$connector->finalize();
+			}
+		}
+		finally
+		{
+			unlink($tmp);
+		}
+	}
+	
+}
+
+?>
